96682d30863d6415a8e3f355d3d3aeeac7fa7c78
anfal17 <mohdanfal17@gmail.com>
2024-05-18 13:40:58 +0530

    added reducers fixed delete

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,25 @@ import AddTodo from "./components/AddTodo/AddTodo";
 import TodoList from "./components/TodoList/TodoList";
 import { useState } from "react";
 import TodoContext from "./context/TodoContext";
+import todoReducer from "./reducers/todoReducer";
+import { useReducer } from "react";
+import TodoDispatchContext from "./context/TodoDispatchContext";
 
 function App() {
-  const[list , setList] = useState([
-    {id:1, todoData:'todo 1' , finished:false},
-    {id:2, todoData:'todo 2' 
-    , finished:true},
-  ])
+  // const[list , setList] = useState([
+  //   {id:1, todoData:'todo 1' , finished:false},
+  //   {id:2, todoData:'todo 2'
+  //   , finished:true},
+  // ])
+
+  const [list, dispatch] = useReducer(todoReducer, []);
 
   return (
-    <TodoContext.Provider value={{list,setList}}>
-      <AddTodo updateList={(todo) => setList([
-        ...list ,
-        {id:(list.length + 1),
-          todoData:todo,
-          finished: false
-        }
-      ])}/>
-      <TodoList />
+    <TodoContext.Provider value={{ list }}>
+      <TodoDispatchContext.Provider value={{ dispatch }}>
+        <AddTodo />
+        <TodoList />
+      </TodoDispatchContext.Provider>
     </TodoContext.Provider>
   );
 }
